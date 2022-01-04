3be78c95ce8c1cfa56692b5f8895f3970edcf3eb
Eslam Elezaby <eslamelezaby98@gmail.com>
2022-01-04 03:38:37 +0200

    adding valation

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -10,59 +10,90 @@ class HomeScreen extends StatefulWidget {
 
 class _HomeScreenState extends State<HomeScreen> {
   late String searchWord = '';
+  final formKey = GlobalKey<FormState>();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
-        crossAxisAlignment: CrossAxisAlignment.center,
-        children: [
-          Padding(
-            padding: const EdgeInsets.only(left: 5, right: 5, top: 10),
-            child: TextFormField(
-              decoration: InputDecoration(
-                hintText: 'put here your error...',
-                enabledBorder: OutlineInputBorder(
-                  borderRadius: BorderRadius.circular(10),
-                ),
-                focusedBorder: OutlineInputBorder(
-                  borderRadius: BorderRadius.circular(10),
-                ),
-              ),
-              onChanged: (value) {
-                searchWord = value;
-              },
-            ),
-          ),
-          const SizedBox(height: 20),
-          ElevatedButton(
-            onPressed: () async {
-              await launch('https://stackoverflow.com/search?q=$searchWord');
-            },
-            child: const Text(
-              'Search',
-            ),
-          ),
-           const SizedBox(height: 20),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              InkWell(
-                onTap: () async {
-                  await launch(
-                      //TODO:  My Github repo
-                      'https://stackoverflow.com/search?q=$searchWord');
-                },
-                child: const Text(
-                  'Get source code at GitHub',
-                  style: TextStyle(
-                    color: Colors.black,
-                    fontSize: 14,
+      body: Form(
+        key: formKey,
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Padding(
+              padding: const EdgeInsets.only(left: 5, right: 5, top: 10),
+              child: TextFormField(
+                decoration: InputDecoration(
+                  hintText: 'put here your error...',
+                  enabledBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                  focusedBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                  errorBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(10),
+                    borderSide: const BorderSide(color: Colors.red),
+                  ),
+                  focusedErrorBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(10),
+                    borderSide: const BorderSide(color: Colors.red),
                   ),
                 ),
+                validator: (value) {
+                  if (value!.isEmpty) {
+                    return 'Required';
+                  } else {
+                    return null;
+                  }
+                },
+                onChanged: (value) {
+                  searchWord = value;
+                },
               ),
-            ],
-          ),
-        ],
+            ),
+            const SizedBox(height: 20),
+            ElevatedButton(
+              style: ElevatedButton.styleFrom(
+                elevation: 2,
+                primary: const Color(0xffDA680B),
+              ),
+              onPressed: () async {
+                if (formKey.currentState!.validate()) {
+                  await launch(
+                      'https://stackoverflow.com/search?q=$searchWord');
+                } else {
+                  await launch('https://www.google.com/');
+                }
+              },
+              child: const Text(
+                'Search',
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 14,
+                ),
+              ),
+            ),
+            const SizedBox(height: 20),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                InkWell(
+                  onTap: () async {
+                    await launch(
+                        'https://github.com/eslamelezaby98/stackoverflow_search');
+                  },
+                  child: const Text(
+                    'Get source code at GitHub',
+                    style: TextStyle(
+                      color: Colors.black,
+                      fontSize: 14,
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ],
+        ),
       ),
     );
   }
